b1f43b64fffeff85233b04218d5ee455916825d2
MikhailChuza <148324632+MikhailChuza@users.noreply.github.com>
2024-02-07 14:24:01 +0200

    3 task ch

--- js/task-3.js
+++ js/task-3.js
@@ -3,22 +3,22 @@ const profile = {
     playTime: 300,
 
     changeUsername(newName) {
-        profile.username = newName;
+        this.username = newName;
     },
 
     updatePlayTime(hours) {
-        profile.playTime += hours;
+        this.playTime += hours;
     },
 
     getInfo() {
-        return `${profile.username} has ${profile.playTime} active hours!`;
+        return `${this.username} has ${this.playTime} active hours!`;
     }
 };
 
-// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-// profile.changeUsername("Marco");
-// console.log(profile.getInfo()); // "Marco has 300 active hours!"
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
-// profile.updatePlayTime(20);
-// console.log(profile.getInfo()); // "Marco has 320 active hours!"
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
